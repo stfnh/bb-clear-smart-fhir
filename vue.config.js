c9b1154976d3d97daef116392ad246fe216afbbc
Stefan Hochwarter <stefan.hochwarter@ntnu.no>
2021-01-10 19:09:29 +0100

    updated vue-cli and eslint

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/bb-clear-smart-fhir/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/bb-clear-smart-fhir/' : '/'
 };
