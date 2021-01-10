5269e6d7d853df5c0a41567e8f44fc009da6513d
Stefan Hochwarter <stefan.hochwarter@ntnu.no>
2021-01-10 20:03:20 +0100

    update babel-jest

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/bb-clear-smart-fhir/' : '/'
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/bb-clear-smart-fhir/' : '/'
 };
